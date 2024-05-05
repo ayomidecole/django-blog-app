3f229e2fe4a4e23296d20dde8d730cad6dea3196
Ayomide Aremu-Cole <121998651+ayomidecole@users.noreply.github.com>
2024-05-05 16:21:04 -0500

    Bug Fix

--- blog/models.py
+++ blog/models.py
@@ -26,12 +26,11 @@ class Post(models.Model):
         default=Status.DRAFT
     )
 
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
-class Meta:
-    ordering = ['-publish']
-    indexes = [
-        models.Index(fields=['-publish']),
-    ]
-
-def __str__(self):
+    def __str__(self):
         return self.title
